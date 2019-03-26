258302bf5bb64b56d9c130247b6772467d0514a6
Steven Thornton <stevenwthornton0@gmail.com>
2019-03-25 19:02:25 -0500

    App is functioning

--- script.js
+++ script.js
@@ -5,19 +5,23 @@ function getDogImages() {
   console.log(pictures);
   fetch(`https://dog.ceo/api/breeds/image/random/${pictures}`)
     .then(response => response.json())
+    // .then(responseJSON => console.log(responseJSON))
     .then(responseJSON => displayResults(responseJSON))
-    // .catch(error => alert("Something went wrong. Try again later"));
+    .catch(error => alert("Something went wrong. Try again later"));
 }
 
 function displayResults(responseJSON) {
+  console.log(responseJSON);
   let pictures = $('input').val();
   const total = parseInt(pictures);
   console.log(total);
+  let index = responseJSON.message;
+  console.log(index);
   for (let i = 0; i < total; i++) {
-    $('images').append(`
-    <img src="${responseJSON.message[i]}" class="results-img">`
-    );
+    $('.images').append(`<img src="${index[i]}" class="results-img">`);
+    console.log(index[i]);
   }
+  $('.results').removeClass('hidden');
 }
 
 function watchForm() {
