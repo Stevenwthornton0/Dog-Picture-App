83405d1e6e16b39b9d56216824e9f47087b59e7c
Steven Thornton <stevenwthornton0@gmail.com>
2019-03-26 18:09:02 -0500

    cleaned up JS

--- script.js
+++ script.js
@@ -3,20 +3,15 @@
 function getDogImages(pictures) {
   fetch(`https://dog.ceo/api/breeds/image/random/${pictures}`)
     .then(response => response.json())
-    // .then(responseJSON => console.log(responseJSON))
     .then(responseJSON => displayResults(responseJSON, pictures))
     .catch(error => alert("Something went wrong. Try again later"));
 }
 
 function displayResults(responseJSON, pictures) {
-  console.log(responseJSON);
   const total = parseInt(pictures);
-  console.log(total);
   let index = responseJSON.message;
-  console.log(index);
   for (let i = 0; i < total; i++) {
     $('.images').append(`<img src="${index[i]}" class="results-img">`);
-    console.log(index[i]);
   }
   $('.results').removeClass('hidden');
 }
